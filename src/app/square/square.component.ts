05f6ab095f3f1d7a45884c3d08cd2bddcc3ae594
Felipe Freitas Silva <eu.felipefreitassilva@gmail.com>
2023-03-23 17:04:21 -0300

    Working PWA

---
+++ src/app/square/square.component.ts
@@ -0,0 +1,36 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-square',
+  template: `
+    <button *ngIf="!value" mat-raised-button style="width: 100%; height: 100%">
+      {{ value }}
+    </button>
+    <button
+      *ngIf="value == 'X'"
+      color="primary"
+      mat-raised-button
+      style="width: 100%; height: 100%"
+    >
+      {{ value }}
+    </button>
+    <button
+      *ngIf="value == 'O'"
+      color="accent"
+      mat-raised-button
+      style="width: 100%; height: 100%"
+    >
+      {{ value }}
+    </button>
+  `,
+  styles: [
+    `
+      button {
+        font-size: 5em;
+      }
+    `,
+  ],
+})
+export class SquareComponent {
+  @Input() value: 'X' | 'O' | null = null;
+}
